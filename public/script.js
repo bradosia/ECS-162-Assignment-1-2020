a2e431754463453e06854a20d8042010fbbf6d10
bradosia <bradosia@gmail.com>
2020-04-10 08:17:23 -0700

    update

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
-document.getElementById("counter").addEventListener("click", ()=>{
-	document.getElementById("counter").textContent = parseInt(document.getElementById("counter").textContent) + 1;
+document.getElementById("counter").addEventListener("click", () => {
+  document.getElementById("counter").textContent = parseInt(document.getElementById("counter").textContent) + 1;
 })
 
-setInterval(function(){
-document.getElementById("counter").textContent = parseInt(document.getElementById("counter").textContent) + 1;
-},1000);
+setInterval(function() {
+  document.getElementById("counter").textContent = parseInt(document.getElementById("counter").textContent) + 1;
+}, 1000);
